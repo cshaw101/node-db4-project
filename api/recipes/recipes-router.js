9f9d8119dd0455705e7caf3ab18955c662e8fae2
caleb <shawcaleb1688@gmail.com>
2023-12-07 15:31:59 -0500

    more progress

--- api/recipes/recipes-router.js
+++ api/recipes/recipes-router.js
@@ -1,14 +1,21 @@
 const router = require('express').Router()
+const Recipe = require('./recipes-model')
 
-router.use('*', (req, res) => {
-    res.json({ api: 'up'})
+
+router.get('/:recipe_id', (req, res, next) => {
+    Recipe.getRecipeById(req.params.recipe_id)
+    .then(resource => {
+        res.status(200).json(resource)
+    })
+    .catch(next)
 })
+
 
 router.use((err, req, res, next) => { // eslint-disable-line
     res.status(500).json({
         customMessage: 'something went wrong inside the recipes router',
         message: err.message,
-        stacl: err.stack,
+        stack: err.stack,
     })
 })
 
